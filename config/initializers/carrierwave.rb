28e6da780722eee39bfc5a1f8d746473a1d9b733
NoritakaTakehara <50361226+NoritakaTakehara@users.noreply.github.com>
2019-11-25 13:32:09 +0900

    carrierwave.rbの修正2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  
+  config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-  config.storage = :fog
   config.fog_directory  = 'noritaka'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/noritaka'
 end
